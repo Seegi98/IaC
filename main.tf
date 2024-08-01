7bb14bb0ed30e9814eeb9c69a5f2d09af1d9defa
Seegi98 <114130893+Seegi98@users.noreply.github.com>
2024-08-01 17:11:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws"{
 
 }
-resource "aws_instance" "useeer" {
+resource "aws_instance" "useettyguer" {
   ami = "ami-068e0f1a600cd311c"
   instance_type = "t2.micro"
   key_name = "cust-user"
